181a6e146b6c581e00a58fad780304b5e6290bd7
poojaghodmode <poojaghodmode@gmail.com>
2021-08-05 12:31:22 +0530

    Fixed secretkey

--- PSAv2/settings.py
+++ PSAv2/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=r9qkr)hu)aq)q2v^wp)p_w_qerv&wb_3zvs3+y#w&ulc-hmpk'
+SECRET_KEY = os.getenv('SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
